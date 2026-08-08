f99d6da9457f346c8b619d7330f3db298030f28d
Hirushan Perera <hirushanperera@Hirushans-MacBook-Air.local>
2026-08-08 14:21:48 +0530

    fix flux assets 404 in production by publishing to public directory

--- routes/console.php
+++ routes/console.php
@@ -6,3 +6,17 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Artisan::command('flux:publish-assets', function () {
+    $filesystem = new \Illuminate\Filesystem\Filesystem;
+    $filesystem->ensureDirectoryExists(public_path('flux'));
+    
+    if ($filesystem->exists(base_path('vendor/livewire/flux/dist'))) {
+        $filesystem->copyDirectory(base_path('vendor/livewire/flux/dist'), public_path('flux'));
+    }
+    if ($filesystem->exists(base_path('vendor/livewire/flux-pro/dist'))) {
+        $filesystem->copyDirectory(base_path('vendor/livewire/flux-pro/dist'), public_path('flux'));
+    }
+    
+    $this->info('Flux assets published successfully.');
+})->purpose('Publish Flux UI assets to public directory');
